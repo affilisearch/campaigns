72df30eb0db79cd17c5450ca666acb7c3266f10c
affilisearch <ck@affilisearch.com>
2026-04-15 11:47:20 +0200

    Update page.js

--- vercel-onepager/app/page.js
+++ vercel-onepager/app/page.js
@@ -1,7 +1,7 @@
 import './globals.css';
 
 const SHEET_CSV_URL =
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vTPnWI_pVzKgZOWAvVEAaSaJsob_oUNMOf4ZukA6ScOzMtkw4wIyUE7GCTorH7PSZYsoNitYDuT8d8Y/pub?gid=1075171072&single=true&output=csv';
+  "/api/sheet";
 
 function parseCsvLine(line) {
   const result = [];
