c9851e1c418be595604b62726acf0aafac43d443
affilisearch <ck@affilisearch.com>
2026-04-15 12:04:34 +0200

    Create route.js

---
+++ vercel-onepager/app/api/sheet/route.js
@@ -0,0 +1,17 @@
+export async function GET() {
+  const url =
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTPnWI_pVzKgZOWAvVEAaSaJsob_oUNMOf4ZukA6ScOzMtkw4wIyUE7GCTorH7PSZYsoNitYDuT8d8Y/pub?gid=1075171072&single=true&output=csv";
+
+  try {
+    const res = await fetch(url);
+    const text = await res.text();
+
+    return new Response(text, {
+      headers: {
+        "Content-Type": "text/plain; charset=utf-8",
+      },
+    });
+  } catch (err) {
+    return new Response("Error", { status: 500 });
+  }
+}
